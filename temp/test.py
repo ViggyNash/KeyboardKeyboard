635289764126ce354f4613c02624d9717874158b
ViggyNash <viggyviggynash@gmail.com>
2016-10-08 21:23:37 -0400

    Changed local and remote directory structures

---
+++ temp/test.py
@@ -0,0 +1,14 @@
+import scipy.io.wavfile as wavfile
+import numpy as np
+import pylab as pl
+rate, data = wavfile.read('file.wav')
+t = np.arange(len(data[:,0]))*1.0/rate
+pl.plot(t, data[:,0])
+pl.show()
+
+p = 20*np.log10(np.abs(np.fft.rfft(data[:2048, 0])))
+f = np.linspace(0, rate/2.0, len(p))
+pl.plot(f, p)
+pl.xlabel("Frequency(Hz)")
+pl.ylabel("Power(dB)")
+pl.show()
